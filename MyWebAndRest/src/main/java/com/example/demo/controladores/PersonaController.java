17d1e7b0a0b4ca099417e3b4d8c5513607480efc
Curso <Curso@PORT-Z5.ipartekaula.com>
2019-10-14 14:24:48 +0200

    Add y Get One funcionando

--- MyWebAndRest/src/main/java/com/example/demo/controladores/PersonaController.java
+++ MyWebAndRest/src/main/java/com/example/demo/controladores/PersonaController.java
@@ -1,10 +1,16 @@
 package com.example.demo.controladores;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.modelos.Persona;
 import com.example.demo.repositorios.IPersonaRepo;
 
 @Controller
@@ -13,8 +19,29 @@ public class PersonaController {
 	private IPersonaRepo repo;
 	
 	@GetMapping("/personas")
-	public String personas(Model model) {
+	public String GetAll(Model model) {
 		model.addAttribute("personas",repo.findAll());
-		return "hola";
+		return "personas";
+	}
+	@GetMapping("/persona/{id}")
+	public String GetOne(@PathVariable("id") Integer id,Model model) {
+		Persona persona = repo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+	     
+		model.addAttribute("persona",persona);
+		return "persona";
+	}
+	@GetMapping("/persona")
+	public String FormAdd() {
+		
+		return "FormAddPersona";
+	}
+	@PostMapping("persona")
+	public String SavePersona(@RequestParam(name="nombre") String nombre,@RequestParam(name="apellidos") String apellidos,@RequestParam(name="edad") Integer edad) {
+		Persona persona = new Persona();
+		persona.setNombre(nombre);
+		persona.setApellidos(apellidos);
+		persona.setEdad(edad);
+		repo.save(persona);
+		return "redirect:/personas";
 	}
 }
